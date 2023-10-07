1ca3bdc6eb895d212723d32943622d98851bfb2c
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-10-07 09:49:27 -0600

    Add react and typescript configuration

---
+++ src/test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export const Test = () => {
+    return React.createElement("h1", null, "Hello World");
+};
---
+++ src/test.tsx
@@ -0,0 +1,5 @@
+import React, { FC, ReactElement } from 'react';
+
+export const Test: FC = (): ReactElement => {
+  return <h1>Hello World</h1>;
+};
